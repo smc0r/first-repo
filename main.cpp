1422870ac4e24e009804bb10046414931f228b01
hs2526 <smcor41@gmail.com>
2024-10-23 18:03:37 +0300

    class whyudoubtedme added

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+class whyudoubtedme
+{
+public:
+	static bool dreams = false;
+	static bool is_there_any_dream();	
+};
+
+bool whyudoubtedme::is_there_any_dream()
+{
+	return false;
+};
+
+int main()
+{
+	bool sameold = whyudoubtedme::is_there_any_dream();
+};
